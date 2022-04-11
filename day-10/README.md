8d48510a013174a0a5d2e84d6866e0462b7361b0
zkan <zkan.cs@gmail.com>
2022-04-11 07:31:18 +0700

    Play with selectbox

---
+++ day-10/README.md
@@ -0,0 +1,11 @@
+# st.selectbox
+
+Display a select widget.
+
+```sh
+streamlit run app.py
+```
+
+Here is the result:
+
+![st.selectbox](./img/st-selectbox.png)
---
+++ day-10/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+st.header("st.selectbox")
+
+option = st.selectbox(
+    "What is your favorite color?",
+    ("Blue", "Red", "Green"),
+)
+st.write("Your favorite color is ", option)
